fa5b2e7ca25c72bab1d195579c8690329a26995e
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-12-11 15:00:16 +0100

    cpp: remove unused macros in fwd.hpp

--- include/nanoeigenpy/fwd.hpp
+++ include/nanoeigenpy/fwd.hpp
@@ -13,37 +13,6 @@
 #include <Eigen/IterativeLinearSolvers>
 #include <Eigen/Jacobi>
 
-#if defined(__clang__)
-#define NANOEIGENPY_CLANG_COMPILER
-#elif defined(__GNUC__)
-#define NANOEIGENPY_GCC_COMPILER
-#elif defined(_MSC_VER)
-#define NANOEIGENPY_MSVC_COMPILER
-#endif
-
-#if __has_include(<cholmod.h>)
-#define NANOEIGENPY_HAS_CHOLMOD
-#endif
-
-#if __has_include(<Accelerate.h>)
-#define NANOEIGENPY_HAS_ACCELERATE
-#endif
-
-#if (__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L))
-#define NANOEIGENPY_WITH_CXX20_SUPPORT
-#endif
-
-#define NANOEIGENPY_UNUSED_TYPE(Type) (void)(Type*)(NULL)
-
-#define NANOEIGENPY_MAKE_TYPEDEFS(Type, Options, TypeSuffix, Size, SizeSuffix) \
-  /** \ingroup matrixtypedefs */                                               \
-  typedef Eigen::Matrix<Type, Size, Size, Options>                             \
-      Matrix##SizeSuffix##TypeSuffix;                                          \
-  /** \ingroup matrixtypedefs */                                               \
-  typedef Eigen::Matrix<Type, Size, 1> Vector##SizeSuffix##TypeSuffix;         \
-  /** \ingroup matrixtypedefs */                                               \
-  typedef Eigen::Matrix<Type, 1, Size> RowVector##SizeSuffix##TypeSuffix;
-
 namespace nanoeigenpy {
 namespace nb = nanobind;
 }  // namespace nanoeigenpy
